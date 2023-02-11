4795bdac217d3092bdd72dd17e300b61e70870ed
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2023-02-11 17:28:38 +0700

    first commit

---
+++ src/components/UsersList.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
+
+function UsersList() {
+  const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>Error fetching data...</div>;
+
+  return <div>{data.length}</div>;
+}
+
+export default UsersList;
